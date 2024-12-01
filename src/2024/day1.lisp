7f2f7698d9065c790b625049e851ebd0a459684b
DarrenN <info@v25media.com>
2024-12-01 17:56:49 -0500

    Adds notes and moves things around for 2024/day1

--- src/2024/day1.lisp
+++ src/2024/day1.lisp
@@ -6,7 +6,18 @@
   (:export #:1a #:1b))
 (in-package :cl-aoc/2024/day1)
 
-;; https://adventofcode.com/2024/day/1
+;;; AoC Day 1
+;;; https://adventofcode.com/2024/day/1
+;;;
+;;; Note: I'm in the process of learning Common Lisp while doing these puzzles.
+;;;
+;;; Broke each answer into an initial step reading the file and converting into
+;;; the appropriate data structures. Then in the 1a and 1b functions loop over
+;;; those data structures to sum up the answers. Really leaned into the loop
+;;; macro to do the summing.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 1a
 
 (defun process-input (filename)
   "Convert input into two lists, sorted by <"
@@ -20,6 +31,17 @@
             ls :initial-value '(() ()))))
     `(,(sort (copy-seq (car ns)) #'<)
       ,(sort (copy-seq (cadr ns)) #'<))))
+
+(defun 1a (filename)
+  "Loop over the two lists subtracting left from right, and summing that value."
+  (let ((input (process-input filename)))
+    (loop :for a :in (car input)
+          :for b :in (cadr input)
+          :summing (abs (- a b)) into total
+          :finally (return total))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 1b
 
 (defun process-input2 (filename)
   "Convert input into a hash-table keyed on right values storing a frequency
@@ -40,22 +62,6 @@ to pull values from the hash-table."
                 (append `(,l) acc)))
             ls :initial-value '())))
     (values h ns)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 1a
-
-(defun 1a (filename)
-  "Loop over the two lists subtracting left from right, and summing that value."
-  (let ((input (process-input filename)))
-    (loop :for a :in (car input)
-          :for b :in (cadr input)
-          :summing (if (< a b)
-                       (- b a)
-                       (- a b)) into total
-          :finally (return total))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 1b
 
 (defun 1b (filename)
   "Loop over the left values, extracting the frequency count from hash-table,
