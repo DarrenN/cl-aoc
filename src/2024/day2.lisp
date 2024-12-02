52d8e83403ad4d40ba28d9163c9f74458ba3052d
DarrenN <info@v25media.com>
2024-12-02 18:45:43 -0500

    Solves part 1

--- src/2024/day2.lisp
+++ src/2024/day2.lisp
@@ -1,4 +1,4 @@
-(defpackage cl-aoc/2024/day1
+(defpackage cl-aoc/2024/day2
   (:use :cl)
   (:import-from :str)
   (:import-from :serapeum #:dict)
@@ -15,12 +15,35 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; part1
 
+(defun sliding-window (lst window-size)
+  "Generate a list of sliding windows of size WINDOW-SIZE from LST."
+  (let ((result 1))
+    (when (and lst (> window-size 0))
+      (loop for i from 0 to (- (length lst) window-size)
+            do (let* ((w (subseq lst i (+ i window-size)))
+                      (s (abs (- (car w) (cadr w)))))
+                 (if (and (< s 4) (> s 0))
+                     result
+                     (setf result 0)))))
+    result))
+
 (defun process-input (filename)
-  (let* ((ls (uiop:read-file-lines filename)))
-    ls))
+  (let* ((ls (uiop:read-file-lines filename))
+         (reports
+           (reduce
+            (lambda (acc rs)
+              (let* ((r (loop :for s :in (str:split " " rs :omit-nulls t)
+                              :collect (parse-number s)))
+                     (v (or (apply #'< r) (apply #'> r))))
+                ;; now check any v = T reports for inc/dec within bounds
+                (if v
+                    (+ acc (sliding-window r 2))
+                    acc)))
+            ls :initial-value 0)))
+    reports))
 
 (defun part1 (filename)
-  filename)
+  (process-input filename))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; part2
